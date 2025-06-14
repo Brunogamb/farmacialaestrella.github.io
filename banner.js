e8310c73d13ac818353ba98d5a12f945f0c7357e
Brunogamb <brunogamberini24@gmail.com>
2025-06-14 14:12:21 -0300

    bannervideo

--- banner.js
+++ banner.js
@@ -22,4 +22,47 @@ document.addEventListener("DOMContentLoaded", () => {
         link.appendChild(img);
         container.appendChild(link);
     });
+
+    const videos = [
+        {
+            src: "VIDEO/videofarmaonline.mp4",
+            enlace: "https://www.farmaonline.com/",
+            alt: "Video destacado"
+        },
+    ];
+
+    videos.forEach(({ src, enlace, alt }) => {
+        const link = document.createElement("a");
+        link.href = enlace;
+        link.className = "card-banner";
+        link.target = "_blank";
+
+        const video = document.createElement("video");
+        video.src = src;
+        video.className = "card-video";
+        video.setAttribute("alt", alt);
+        video.setAttribute("autoplay", "");
+        video.setAttribute("muted", "");
+        video.setAttribute("playsinline", "");
+        video.setAttribute("loop", "");
+        video.muted = true;
+
+        link.appendChild(video);
+        container.appendChild(link);
+
+        const observer = new IntersectionObserver(
+            (entries) => {
+                entries.forEach(entry => {
+                    if (entry.isIntersecting) {
+                        video.play();
+                    } else {
+                        video.pause();
+                        video.currentTime = 0;
+                    }
+                });
+            },
+            { threshold: 0.5 }
+        );
+        observer.observe(video);
+    });
 });
